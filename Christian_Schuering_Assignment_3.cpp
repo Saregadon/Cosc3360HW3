7876939157d2ed46900f1f9569fe4e076e58088f
Christian S <Saregadon@gmail.com>
2021-04-28 15:59:59 -0500

    hw3 first push

---
+++ Christian_Schuering_Assignment_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <pthread.h>
+
+using namespace std;
+
+int main()
+{
+    //ifstream input;
+
+
+
+    string name;
+    int elapsedtime = 0, waittime = 0, safetyprecaution = 0;
+
+    while(cin >> name >> elapsedtime >> waittime >> safetyprecaution)
+    {
+
+    }
+}
